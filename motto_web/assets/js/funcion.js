cf3ac47f453849042f068b1aa06ca8e6d3908690
juhankim-mjt <juhan.kim.mjt@gmail.com>
2025-04-09 14:41:41 +0900

    fix

--- motto_web/assets/js/funcion.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('myCheckbox').addEventListener('change', function () {
-  const label = document.getElementById('mytickets');
-  if (this.checked) {
-    label.classList.add('strike');
-  } else {
-    label.classList.remove('strike');
-  }
-});
